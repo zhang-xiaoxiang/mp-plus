2d7764008c8090e2e9e03ecec21019a62af245b1
zhangxiaoxiang <1090510535@qq.com>
2019-07-02 18:28:59 +0800

    mp demo

--- src/main/java/com/example/plus/MpPlusApplication.java
+++ src/main/java/com/example/plus/MpPlusApplication.java
@@ -1,13 +1,21 @@
-package com.example.mpplus;
+package com.example.plus;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
+
+/**
+ * MpPlusApplication:
+ * @author zhangxiaoxiang
+ * @date 2019/7/2
+ */
 @Slf4j
 @Configuration
 @EnableAutoConfiguration
+@MapperScan("com.example.plus.dao")
 public class MpPlusApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/example/plus/MpPlusApplicationTests.java
+++ src/test/java/com/example/plus/MpPlusApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.mpplus;
+package com.example.plus;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
