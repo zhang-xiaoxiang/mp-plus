312e37eaf5197b5990d96fab0cbabd837eb05c7e
zhangxiaoxiang <1090510535@qq.com>
2019-07-04 09:07:01 +0800

    更新代码

---
+++ src/main/java/com/example/plus/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.plus.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 客户端用户表 前端控制器
+ * </p>
+ *
+ * @author zhangxiaoxiang
+ * @since 2019-07-03
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+}
